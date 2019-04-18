55825f7107e34f753476030bcf9cea638a6682b7
Yang Xu <xy6802@live.com>
2019-04-18 10:51:15 +0800

    Upload

---
+++ ISBN/ISBN.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ISBN.aspx.cs" Inherits="ISBN.ISBN" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        tr{
+           
+        }
+        td{
+            width:50%;
+            padding-top:10px;
+        }
+        .button{
+            width:100px;
+            height:25px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="width:100%;text-align:center;padding-top:100px;">
+             <h1>Standard ISBN-10 Conversion</h1>
+            <table style="width:50%; margin:auto;">
+                <tr>
+                    <td>
+                        Product ID :
+                    </td>
+                    <td style="text-align:left;">
+                        <asp:TextBox ID="ProductIDTextBox" runat="server" MaxLength="12" Width="150px"></asp:TextBox>
+                        <asp:Label ID="LabelError" runat="server" Font-Size="XX-Small" ForeColor="Red" Text="12 digits only" Visible="False"></asp:Label>
+            <br />
+                    </td>
+                </tr>
+               
+                <tr>
+                    <td>
+                        <asp:Label ID="LabelISBN" runat="server" Text="ISBN-10 :" Visible="False"></asp:Label>
+                    &nbsp;</td>
+                    <td style="text-align:left;">
+                        <asp:Label ID="ISBNnum" runat="server" Visible="False"></asp:Label>
+                    </td>
+                </tr>
+                 <tr>
+                    <td style="text-align:right;padding-right:30px;">
+                         <asp:Button ID="ButtonConvert" class="button" runat="server" OnClick="ButtonConvert_Click" Text="Convert" />
+                    </td>
+                    <td style="text-align:left;padding-left:30px;">
+                        <asp:Button ID="ButtonClear" class="button" runat="server" Text="Clear" OnClick="ButtonClear_Click" />
+
+                    </td>
+                </tr>
+            </table>
+            
+            <br />
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ ISBN/ISBN.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ISBN
+{
+    public partial class ISBN : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        //convert method
+        private static string GetIsbn(string productID)
+        {
+            //Remove(978)
+            string isbn9 = productID.Substring(3);
+            char[] isbn = isbn9.ToCharArray();
+            int j = 10;
+            int sum = 0;
+            for (int i = 0; i < isbn.Length; i++)
+            {
+                sum += Convert.ToInt32(isbn[i].ToString()) * j;
+                j--;
+            }
+            int a = 11 - (sum % 11);
+            //a<10 ->a 
+            //a =10 -> x 
+            //a =11 -> 0
+            string isbn10 = (a < 10) ? a.ToString() : ((a == 10) ? "x" : "0");
+            return isbn9 + isbn10;
+
+        }
+        //Validation
+        private bool checkIDvalidation(string productID)
+        {
+            bool validate = true;
+            if(!Int64.TryParse(productID,out long ID) || productID.Length != 12)
+            // 
+            {
+                LabelError.Visible = true;
+                validate = false;
+            }
+            return validate;
+        }
+        //Convert button
+        protected void ButtonConvert_Click(object sender, EventArgs e)
+        {
+            if (checkIDvalidation(ProductIDTextBox.Text))
+            {
+                ISBNnum.Text = GetIsbn(ProductIDTextBox.Text);
+
+                LabelError.Visible = false;
+                LabelISBN.Visible = true;
+                ISBNnum.Visible = true;
+            }
+            else
+            {
+                LabelISBN.Visible = false;
+                ISBNnum.Visible = false;
+            }
+        }
+        //Clear Button
+        protected void ButtonClear_Click(object sender, EventArgs e)
+        {
+            ProductIDTextBox.Text = string.Empty;
+            ISBNnum.Text = string.Empty;
+            ISBNnum.Visible = false;
+            LabelError.Visible = false;
+            LabelISBN.Visible = false;
+        }
+    }
+}
